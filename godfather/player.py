4ff77ad3a122950d906623a827cc80a94cb9a8c4
jupiter24 <jupiter24@users.noreply.github.com>
2017-07-25 16:20:35 +0200

    Conform to PEP8 and Google-style docstrings
    The following changes were made to conform to stilistic standards:
    1. Use """ for docstrings
    2. Don't mention methods in class docstrings
    3. Use Google standard for docstrings
    4. Rename player.assignrole to player.assign_role
    5. Use imperative for method descriptions
    6. Use # for multiline comments
    
    Furthermore, type checking in methods has been removed because the methods in question shouldn't be used outside this project anyways

--- godfather/player.py
+++ godfather/player.py
@@ -1,49 +1,45 @@
-''' This module contains a class which is used to represent the participating players in a game.'''
+""" This module contains a class which is used to represent the participating players in a game."""
 
 import telegram
 
 import godfather.role
 
+
 class Player:
-    ''' Every instance of the class Player represents one player in the game.
-        The instance is initialized with information about the user itself;
-        game related information is added later.
+    """ Every instance of the class Player represents one player in the game.
+
+    The instance is initialized with information about the user itself;
+    game related information is added later.
+
+    Args:
+        telegram_user (telegram.user.User): An object from the Telegram API
+            to identify the Telegram account of the player
         
-        Attributes:
-            user (telegram.user.User) -- Specific information about the actual user
-            role (role.Role)          -- All game related information is saved here
-            
-        Methods:
-            assignrole(self, role)    -- Sets self.role to a given role
-    '''
-
+    Attributes:
+        user (telegram.user.User): Specific information about the actual user
+        role (role.Role): All game related information is saved here
+    """
     def __init__(self, telegram_user):
+        #  telegram.user.User Objects contain the following arguments:
 
-        if isinstance(id, telegram.user.User): 
-            self.user = telegram_user
-            ''' telegram.user.User Objects contain the following arguments:
-
-                id              -- unique ID that is required to send text messages
-                language_code   -- language code chosen by the given user
-                first_name      -- first name of user
-                last_name       -- last name of user (optional)
-                username        -- unique Telegram tag (optional)
-                name            -- equals username if available (with @ sign), 
-                                   otherwise first name + last name
-            '''
-        else:
-            raise TypeError("A telegram-user object must be delivered")
-
+        # id              -- unique ID that is required to send text messages
+        # language_code   -- language code chosen by the given user
+        # first_name      -- first name of user
+        # last_name       -- last name of user (optional)
+        # username        -- unique Telegram tag (optional)
+        # name            -- equals username if available (with @ sign),  
+        #                    otherwise first name + last name
+        self.user = telegram_user
         self.role = None
 
-    def assignrole(self, role):
-        ''' Sets self.role to a given role. 
-        
-            Is triggered in the pre-game phase for every single player and might be used by certain
-            abilities within games.
-        ''' 
 
-        if isinstance(role, godfather.role.Role):
-            self.role = role
-        else:
-            raise TypeError("Roles must be of type Role")
+    def assign_role(self, role):
+        """ Sets the role of the player to a given role.
+        
+        This is triggered in the pre-game phase for every single player and might be used by certain
+        abilities within games.
+
+        Args:
+            role (role.Role): The new role the player should have
+        """ 
+        self.role = role
